873d1a58aec196891c0cf1179c5f9af31487213a
Sachin Kumar <sachin.kumar1@daffodilsw.com>
2023-09-04 14:53:37 +0530

    file structure fixed and added icons and font

---
+++ lib/common/status_bar.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+class CustomeStatusBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomeStatusBar({super.key});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(90);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      elevation: 0.0,
+      title: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(left: 10),
+                child: Image.network(
+                  'https://static-00.iconduck.com/assets.00/myntra-icon-2048x1386-nda4rc65.png',
+                  width: 35,
+                  height: 35,
+                ),
+              ),
+              Row(
+                children: [
+                  InkWell(
+                    borderRadius: BorderRadius.circular(3000.0),
+                    splashColor: Colors.black.withOpacity(0.3),
+                    onTap: () {
+                      // Handle your onTap here.
+                      if (kDebugMode) {
+                        print('Image tapped!');
+                      }
+                    },
+                    child: Image.asset(
+                      'assets/icons/notification_icon.png',
+                      height: 30,
+                      width: 30,
+                    ),
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.favorite_border),
+                    color: Colors.black,
+                    onPressed: () {
+                      if (kDebugMode) {
+                        print('Notification button pressed');
+                      }
+                    },
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.card_travel),
+                    color: Colors.black,
+                    onPressed: () {
+                      if (kDebugMode) {
+                        print('Notification button pressed');
+                      }
+                    },
+                  ),
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+      bottom: PreferredSize(
+        preferredSize: const Size.fromHeight(30.0), // adjust the size as needed
+        child: Padding(
+          padding: const EdgeInsets.all(10.0), // adjust the padding as needed
+          child: Container(
+            height: 30,
+            margin: const EdgeInsets.symmetric(horizontal: 5),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding:
+                    const EdgeInsets.symmetric(vertical: 4.0, horizontal: 10),
+                hintText: 'Search',
+                hintStyle: const TextStyle(fontFamily: "CustomFontFamily"),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(
+                      35.0), // Increase this value to increase the border radius
+                ),
+                focusedBorder:   const OutlineInputBorder(borderSide: BorderSide(width: 1, color: Colors.black),borderRadius: BorderRadius.all(Radius.circular(30))),
+              ),
+            ),
+          ), // replace 'sdsd' with your text
+        ),
+      ),
+      backgroundColor: Colors.white,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
+import 'package:shopping_flutter/common/status_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,88 +16,6 @@ class MyApp extends StatelessWidget {
         appBar: CustomeStatusBar(),
         body: Center(child: Text("dd")),
       ),
-    );
-  }
-}
-
-class CustomeStatusBar extends StatelessWidget implements PreferredSizeWidget {
-  const CustomeStatusBar({super.key});
-
-  @override
-  Size get preferredSize => const Size.fromHeight(90);
-
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      elevation: 0.0,
-      title: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                margin: const EdgeInsets.only(left: 10),
-                child: Image.network(
-                  'https://static-00.iconduck.com/assets.00/myntra-icon-2048x1386-nda4rc65.png',
-                  width: 35,
-                  height: 35,
-                ),
-              ),
-              Row(
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.notifications),
-                    color: Colors.black,
-                    onPressed: () {
-                      if (kDebugMode) {
-                        print('Notification button pressed');
-                      }
-                    },
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.favorite_border),
-                    color: Colors.black,
-                    onPressed: () {
-                      if (kDebugMode) {
-                        print('Notification button pressed');
-                      }
-                    },
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.card_travel),
-                    color: Colors.black,
-                    onPressed: () {
-                      if (kDebugMode) {
-                        print('Notification button pressed');
-                      }
-                    },
-                  ),
-                ],
-              )
-            ],
-          ),
-        ],
-      ),
-      bottom: PreferredSize(
-        preferredSize: const Size.fromHeight(30.0), // adjust the size as needed
-        child: Padding(
-          padding: const EdgeInsets.all(10.0), // adjust the padding as needed
-          child: Container(
-                height: 30,
-                margin: const EdgeInsets.symmetric(horizontal: 5),
-                child: TextField(
-                  decoration: InputDecoration(
-                     contentPadding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 10),
-                    hintText: 'Search',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(35.0), // Increase this value to increase the border radius
-                  ),
-                ),
-              ),
-              ),// replace 'sdsd' with your text
-        ),
-      ),
-      backgroundColor: Colors.white,
     );
   }
 }
