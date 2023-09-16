728687dfd1905aab56b62d42be1e30e8564836d2
Sachin Kumar <sachin.kumar1@daffodilsw.com>
2023-09-16 18:54:17 +0530

    fix

--- lib/newArchitecture/presentation/screens/home/home.dart
+++ lib/newArchitecture/presentation/screens/home/home.dart
@@ -3,8 +3,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shopping_flutter/newArchitecture/presentation/common_widgets/home_status_bar.dart';
 import 'package:shopping_flutter/newArchitecture/presentation/common_widgets/product_container.dart';
 import 'package:shopping_flutter/newArchitecture/presentation/screens/sign_in_screen/bloc/sign_in_bloc.dart';
+import 'package:shopping_flutter/newArchitecture/presentation/screens/sign_in_screen/bloc/sign_in_event.dart';
 import 'package:shopping_flutter/newArchitecture/presentation/screens/sign_in_screen/bloc/sign_in_state.dart';
 import 'package:shopping_flutter/newArchitecture/common_bloc/customer_profile_bloc/customer_profile_bloc.dart';
+import 'package:shopping_flutter/newArchitecture/common_bloc/customer_profile_bloc/customer_profile_state.dart';
 import 'package:shopping_flutter/newArchitecture/common_bloc/customer_profile_bloc/customer_profile_event.dart';
 
 class Home extends StatefulWidget {
@@ -24,18 +26,27 @@ class _HomeState extends State<Home> {
         }
       },
       builder: (context, state) {
-        return Scaffold(
-          appBar: const HomeStatusBar(),
-          body: GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              childAspectRatio: 0.6,
-            ),
-            itemCount: 5,
-            itemBuilder: (context, index) {
-              return const ProductContainer();
-            },
-          ),
+        return BlocConsumer<CustomerProfileBloc, CustomerProfileState>(
+          listener: (context, state) {
+              if(state is CustomerProfileErrorState){
+                  context.read<SignInBloc>().add(SignOutEvent());
+              }
+          },
+          builder: (context, state) {
+            return Scaffold(
+              appBar: const HomeStatusBar(),
+              body: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 0.6,
+                ),
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return const ProductContainer();
+                },
+              ),
+            );
+          },
         );
       },
     );
